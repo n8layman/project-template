4fa3503dec34da58a2bf539236847d63838340ff
Nathan Layman <layman@ecohealthalliance.org>
2025-02-09 22:56:37 -0800

    Update daily note

--- R/get_connected_targets.R
+++ R/get_connected_targets.R
@@ -1,10 +1,15 @@
 get_connected_targets <- function(target_raw, mode = "downstream") {
   # Maybe make faster like tar_glimpse?
-  if(!mode %in% c("upstream", "downstream")) stop("mode must be either 'upstream' or 'downstream'")
+  if (!mode %in% c("upstream", "downstream")) {
+    stop("mode must be either 'upstream' or 'downstream'")
+  }
   mode <- ifelse(mode == "upstream", "in", "out")
-  network <- targets::tar_network(targets_only = T)
-  g <- igraph::graph_from_data_frame(d = network$edges, directed = T)
-  connections <- igraph::all_simple_paths(g, target_raw, mode = mode) # This is downstream targets. “in” is upstream
-  connected_targets = unlist(connections) |> names() |> unique()
+  network <- targets::tar_network(targets_only = TRUE)
+  g <- igraph::graph_from_data_frame(d = network$edges, directed = TRUE)
+  # This is downstream targets. “in” is upstream
+  connections <- igraph::all_simple_paths(g, target_raw, mode = mode)
+  connected_targets <- unlist(connections) |>
+    names() |>
+    unique()
   connected_targets
 }
